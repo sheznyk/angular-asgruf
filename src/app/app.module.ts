949fdbd06c0e73d7332b72c36e2b265f2caea8b9
sheznyk <karis.brivaus@gmail.com>
2019-12-29 22:32:40 +0200

    2 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { ToDoDetailsComponent } from './components/to-do-details/to-do-details.component';
+
+const appRoutes: Routes = [
+  { path: 'to-do-list', component: ToDoListComponent },
+  { path: 'to-do-details/:id',      component: ToDoDetailsComponent },
+  { path: '',
+    redirectTo: '/to-do-list',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, ToDoListComponent, ToDoDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/to-do-details/to-do-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-details',
+  templateUrl: './to-do-details.component.html',
+  styleUrls: ['./to-do-details.component.css']
+})
+export class ToDoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
